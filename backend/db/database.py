09ebcb14836b0232c2df2ed972316f132eba8dd1
tallomb <tallomb66@gmail.com>
2024-11-03 22:44:50 +0200

    or

--- backend/db/database.py
+++ backend/db/database.py
@@ -2,6 +2,6 @@ import os
 import motor.motor_asyncio
 from core.config import settings
 
-mongodb_uri = os.getenv("DATABASE_URI") | settings.DATABASE_URL
+mongodb_uri = os.getenv("DATABASE_URI") or settings.DATABASE_URL
 client = motor.motor_asyncio.AsyncIOMotorClient(mongodb_uri)
 db = client["SunDay"]
