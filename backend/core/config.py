db85c63273f572be2930a41d418e2c5ff16c2055
tallomb <tallomb66@gmail.com>
2024-11-03 22:41:28 +0200

    fix

--- backend/core/config.py
+++ backend/core/config.py
@@ -32,21 +32,5 @@ class Settings:
     # משתמש בדיקות
     TEST_USER_EMAIL = "test@example.com"  # אימייל של משתמש לבדיקה במערכת
 
-    # הגדרות למסד הנתונים - משתני הסביבה שמוגדרים בדוקר קומפוס
-    DATABASE_USER: str = os.getenv("DATABASE_USER")
-    DATABASE_PASSWORD: str = os.getenv("DATABASE_PASSWORD")
-    DATABASE_HOST: str = os.getenv("DATABASE_HOST")
-    DATABASE_PORT: str = os.getenv("DATABASE_PORT")
-
-    # כתובת חיבור למסד הנתונים, שנבנית באמצעות פרטי החיבור   
-    DATABASE_URL = "mongodb://{user}:{password}@{host}:{port}".format(user=DATABASE_USER, password=DATABASE_PASSWORD, host=DATABASE_HOST, port=DATABASE_PORT)
-    
-    # הגדרות לאבטחת ה-API
-    SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7" 
-    ALGORITHM = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-    TEST_USER_EMAIL = "test@example.com"
-
 
 settings = Settings()
--- backend/db/database.py
+++ backend/db/database.py
@@ -1,6 +1,7 @@
+import os
 import motor.motor_asyncio
 from core.config import settings
 
-mongodb_uri = settings.DATABASE_URL
+mongodb_uri = os.getenv("DATABASE_URI") | settings.DATABASE_URL
 client = motor.motor_asyncio.AsyncIOMotorClient(mongodb_uri)
 db = client["SunDay"]
